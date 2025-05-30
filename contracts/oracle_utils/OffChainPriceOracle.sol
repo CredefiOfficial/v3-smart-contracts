afb1993e44b15ba7f78a66e8a11b1f46ddf7279d
krumbgf <krumbgf@gmail.com>
2025-05-30 14:23:47 +0300

    OffChainOracle: add chain_id, rem _last_iat

--- contracts/oracle_utils/OffChainPriceOracle.sol
+++ contracts/oracle_utils/OffChainPriceOracle.sol
@@ -7,8 +7,8 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 abstract contract OffChainPriceOracle
 {
     mapping (address => bool) private _signers;
-    mapping (bytes32 => uint96) private _last_iat;
-    bytes32 PRICE_DATA_TYPE_HASH = 0x4e24c8b160ad2b512b9e6bd61624e0c216f7fc373cb977ad891206a7db1b9dc1;
+    //mapping (bytes32 => uint96) private _last_iat;
+    bytes32 PRICE_DATA_TYPE_HASH = 0x08c4a6ba964682b11f46df8ca21a2332795b52b556af6ce17e5f0727ca0f8e53;
 
     event SignerChanged(address signer, bool is_authorized);
 
@@ -16,10 +16,12 @@ abstract contract OffChainPriceOracle
     {
         require(data.length > 0, "OffChainOracle:Invalid data!");
         (bytes memory price_data, bytes memory signature) = abi.decode(data, (bytes, bytes));     
-        (bytes32 type_hash, address base, address quote, uint256 price, uint8 decimals, uint96 issued_at, uint96 expiry) = abi.decode(price_data, (bytes32, address, address, uint256, uint8, uint96, uint96));  
+        (bytes32 type_hash, uint256 chain_id, address base, address quote, uint256 price, uint8 decimals, uint96 issued_at, uint96 expiry) = abi.decode(price_data, (bytes32, uint256, address, address, uint256, uint8, uint96, uint96));  
         require(type_hash == PRICE_DATA_TYPE_HASH, "OffChainOracle:Invalid TYPE_HASH!");
-        bytes32 pair_hash = keccak256(abi.encodePacked(base, quote));
-        require(uint96(block.timestamp) <= expiry && _last_iat[pair_hash] <= issued_at, "OffChainOracle:Expired data!");
+        require(chain_id == block.chainid, "OffChainOracle:Invalid chain_id!");
+        //bytes32 pair_hash = keccak256(abi.encodePacked(base, quote));
+        //require(uint96(block.timestamp) <= expiry && _last_iat[pair_hash] <= issued_at, "OffChainOracle:Expired data!");
+        require(uint96(block.timestamp) <= expiry, "OffChainOracle:Expired data!");
         validate_signature(price_data, signature);
         return (base, quote, price, decimals, issued_at, expiry);
     }
@@ -27,7 +29,7 @@ abstract contract OffChainPriceOracle
     function decode_data_and_update_expiry(bytes calldata data) internal returns(address, address, uint256, uint8, uint96, uint96)
     {
         (address base, address quote, uint256 price, uint8 decimals, uint96 issued_at, uint96 expiry) = decode_data(data);
-        _last_iat[keccak256(abi.encodePacked(base, quote))] = issued_at;
+        //_last_iat[keccak256(abi.encodePacked(base, quote))] = issued_at;
         return (base, quote, price, decimals, issued_at, expiry);
     }
 
